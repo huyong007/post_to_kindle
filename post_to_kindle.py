fc53fd0335a5f5d54007e19ec3b56b0c888b9748
wangjie <1142890791@qq.com>
2019-12-15 18:18:23 +0800

    2019-12-15 18:18:23.388483

--- post_to_kindle.py
+++ post_to_kindle.py
@@ -5,6 +5,7 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 import smtplib
 from email.mime.text import MIMEText
+import os 
 
 def _format_addr(s):
     name,addr = parseaddr(s)
@@ -13,6 +14,8 @@ def _format_addr(s):
 # 输入Email地址和口令
 # 检查是否使用默认邮箱
 def check_email(email,type):
+    if email != '':
+        return email
     if (type =='from') and (email == ''):
         print('当前发送邮箱为空，将使用默认邮箱:1341696804@qq.com')
         return  '1341696804@qq.com'
@@ -41,19 +44,25 @@ smtp_server = input('SMTP server: ')
 smtp_server = check_email(smtp_server,'server')
 # 邮件对象
 msg  = MIMEMultipart()
-msg['From'] = _format_addr('Python爱好者<%s>'%from_addr)
+msg['From'] = _format_addr('胡永的python脚本<%s>'%from_addr)
 msg['To'] = _format_addr('管理员<%s>'%to_addr)
 msg['Subject'] = Header('来自SMTP的问候...','utf-8').encode()
 
 # 邮件正文是MIMEText:
 msg.attach(MIMEText('this file sent by huyong`s Python script','plain','utf-8'))
-
+# 列出book文件中的所有文件
+filePath = '/Users/smart/python/book'
+def list_dir_file(now_dir):
+    print('book文件中的文件名\n',[x for x in os.listdir(now_dir) if os.path.isfile(x)])
+list_dir_file(filePath)
+fileName = input('输入book文件夹中要post到kindle的文件名: ')
+print('您要上传的文件名是：',fileName)
 # 添加附件就是加上一个MIMEBase，从本地读取一个图片
-with open('/Users/smart/python/book/七周七语言.pdf','rb') as f:
+with open(os.path.join(filePath,fileName),'rb') as f:
     # 设置附件的MIME和文件名字，这里是jpg类型：
-    mime = MIMEBase('application','pdf',fileName='七周七语言.pdf')
+    mime = MIMEBase('application','pdf',fileName=str(fileName))
     # 加上必要的头信息
-    mime.add_header('Content-Disposition','attachment',filename='七周七语言.pdf')
+    mime.add_header('Content-Disposition','attachment',filename = str(fileName))
     mime.add_header('Content_ID','<0>')
     mime.add_header('X-Attachment-Id','0')
     # 把附件的内容读进来：
