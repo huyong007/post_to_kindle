46931ff5b2666261f18164721103c317fc92eb4f
huyong007 <1341696804@qq.com>
2019-12-19 09:42:36 +0800

    class learn

---
+++ file_type.py
@@ -0,0 +1,10 @@
+class Restaurant():
+   def __init__(self,restaurant_name,cuisine_type):
+       self.restaurant_name = restaurant_name
+       self.cuisine_type = cuisine_type
+    def print_restaurant(self):
+        print(self.restaurant_name +' ' + self.cuisine_type)
+        
+    
+    
+    
