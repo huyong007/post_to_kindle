1211653c37d770687ab0befde6d386d5ae3bca63
huyong007 <1341696804@qq.com>
2019-12-20 16:26:40 +0800

    fix bug

--- file_type.py
+++ file_type.py
@@ -2,8 +2,7 @@ class Restaurant():
    def __init__(self,restaurant_name,cuisine_type):
        self.restaurant_name = restaurant_name
        self.cuisine_type = cuisine_type
-    def print_restaurant(self):
-        print(self.restaurant_name +' ' + self.cuisine_type)
+
         
     
     
