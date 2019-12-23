fc66b8350c16a341d002e7e05780583c5936a344
wangjie <1142890791@qq.com>
2019-12-23 20:52:30 +0800

    add subprocess

--- git_script.py
+++ git_script.py
@@ -1,9 +1,11 @@
 import subprocess
 import datetime
-subprocess.call(['git','init'])
-subprocess.call(['git','add','-A'])
+subprocess.call(['git', 'init'])
+subprocess.call(['git', 'add', '-A'])
 commit_tag = input('Enter this commit tag: ')
 if commit_tag == '':
     commit_tag = str(datetime.datetime.now())
-subprocess.call(['git','commit','-m',str(commit_tag)])
-subprocess.call(['git','push','https://github.com/huyong007/post_to_kindle.git','master'])
+subprocess.call(['git', 'pull'])
+subprocess.call(['git', 'commit', '-m', str(commit_tag)])
+subprocess.call(
+    ['git', 'push', 'https://github.com/huyong007/post_to_kindle.git', 'master'])
